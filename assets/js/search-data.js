ad3285292dd59c6f8793a7a33d254b054c4ba81b
samra-irshad <samra@khu.ac.kr>
2026-03-03 17:57:25 +0000

    Deploying to gh-pages from @ samra-irshad/samra-irshad.github.io@1a3e35f4035b2e1794d4e1fe894bfa53b5ed52c6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -462,7 +462,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%61%6D%72%61@%6B%68%75.%61%63.%6B%72", "_blank");
         },
       },{
         id: 'social-inspire',
