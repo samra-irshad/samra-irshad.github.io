517ad4b38b63ca6ccc1d266e2436ca739d34a29c
samra-irshad <samra@khu.ac.kr>
2026-03-03 17:50:38 +0000

    Deploying to gh-pages from @ samra-irshad/samra-irshad.github.io@caed1a7fa1c14ea8850618c4783a6894fae9ae35 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -458,39 +458,46 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
+        id: 'social-cv',
+        title: 'CV',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%73%61%6D%72%61@%6B%68%75.%61%63.%6B%72", "_blank");
+          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-inspire',
+        title: 'Inspire HEP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=ZVe6sNMAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
-        id: 'social-github',
-        title: 'GitHub',
+        id: 'social-custom_social',
+        title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://github.com/samra-irshad", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/samra-sam-irshad-22329b1b6", "_blank");
-        },
-      },{
-        id: 'social-twitter_username',
-        title: 'Twitter_username',
-        section: 'Socials',
-        handler: () => {
-          window.open("", "_blank");
+          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
